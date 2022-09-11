49acd2c92a6b3f2e113e285f6a8920d8fc83c4bb
Chenhao <afiretony@gmail.com>
2022-09-11 13:24:13 -0400

    ask server for help

--- assignments/assgn1/answer.md
+++ assignments/assgn1/answer.md
@@ -1,12 +1,65 @@
-## 1.1
+# Homework Assignment 1
 
-Loading Nikon D3400 image from campus.nef ...
+15-663, Computational Photography, Fall 2022, Carnegie Mellon University
 
-Scaling with darkness 150, saturation 4095, and
+Chenhao Yang
 
-multipliers 2.394531 1.000000 1.597656 1.000000
+---
 
-Building histograms...
+## 1. Developing RAW images
 
-Writing data to campus.tiff ...
+### 1.1 Implement a basic image processing pipeline (80 points)
 
+#### RAW image conversion (5 points).
+
+| black | white | R scale  | G scale  | B scale  |
+| ----- | ----- | -------- | -------- | -------- |
+| 150   | 4095  | 2.394531 | 1.000000 | 1.597656 |
+
+#### Python initials (5 points).
+
+| BPP  | Width | Height |
+| ---- | ----- | ------ |
+| 16   | 6016  | 4016   |
+
+Check and report how many bits per pixel the image has, its width, and its height. Then, convert the image into a double-precision array
+
+#### Linearization (5 points).
+
+Implemented using `numpy.clip()`.
+
+#### Identifying the correct Bayer pattern (20 points).
+
+
+
+#### White balancing (10 points).
+
+
+
+
+
+#### Demosaicing (10 points).
+
+#### Color space correction (10 points).
+
+#### Brightness adjustment and gamma encoding (10 points).
+
+#### Compression (5 points).
+
+
+
+### 1.2 Perform manual white balancing (10 points)
+
+### 1.3 Learn to use dcraw (10 points)
+
+
+
+## 2. Camera Obscura
+
+
+
+### 2.1 Build the pinhole camera (70 points)
+
+### 2.2 Use your pinhole camera (30 points)
+
+### 2.3 Bonus: Camera obscura in your room (20 points)
---
+++ assignments/assgn1/src/im_processing.py
@@ -0,0 +1,214 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import io
+from scipy.interpolate import interp2d, RectBivariateSpline
+
+def read_RAW(img_path):
+    """
+    read RAW images using skimage
+    """
+    im = io.imread(img_path)
+    return im
+
+def img_info(im):
+    """
+    input: 
+        numpy array
+    output:
+        image info contains [bits per pixel, width, height]
+    """
+    if len(im.shape) == 2:
+        im_info = {"bpp": im.dtype,
+                    "width": im.shape[1],
+                    "height": im.shape[0]
+                    }
+
+    elif len(im.shape) == 3:
+        im_info = {"bpp": im.dtype,
+                    "width": im.shape[2],
+                    "height": im.shape[1]
+                    }
+
+    else:
+        raise ValueError("Check image dimension")
+
+    return im_info
+
+def convert2double(im):
+    """
+    convert the image into a double-precision array
+    """
+    return im.astype(np.double)
+
+def linearize(im, black, white):
+    """
+    Linearize the image using method descripted in 1.1
+    """
+    linearized = np.zeros_like(im)
+    linearized = (im - black) / (white - black)
+    linearized = np.clip(linearized, 0., 1.)
+    return linearized
+
+def identify_bayer_dummy(im, pattern):
+    """
+    based on pattern of choice, return 3-channel(RGB) image 
+    """
+    assert pattern in ['grbg', 'rggb', 'bggr', 'gbrg']
+
+    im_info = img_info(im)
+    h, w = im_info["height"], im_info["width"]
+
+    RGB = np.zeros((h // 2, w // 2, 3))
+    
+    for i in range(h//2):
+        for j in range(w//2):
+            # print(j)
+            RGB[i][j][0] = im[i*2, j*2]
+            RGB[i][j][1] = im[i*2, j*2+1]
+            RGB[i][j][2] = im[i*2+1, j*2+1]
+    
+    return RGB
+
+def demosaicing(im):
+    """
+    demosaicing using pattern rggb
+    """
+    h, w = im.shape[0], im.shape[1]
+    RGB = np.zeros((h, w, 3))
+    grid_x, grid_y = np.meshgrid(np.arange(0, h), np.arange(0, w))
+
+    # Red Channel
+    x = np.arange(0, h, 2)
+    y = np.arange(0, w, 2)
+    xv, yv = np.meshgrid(x, y)
+    R = im[xv.T, yv.T]
+    f = RectBivariateSpline(x, y, R)
+    print(f(0,1))
+
+    
+    # Green Channel
+
+
+    # print(xv.shape, yv.shape, z.shape)
+    # print(z)
+    # f = interp2d(xv, yv, z)
+    # z = f(grid_x, grid_y)
+    # print(z)
+    return
+
+    
+
+def identify_bayer(im, pattern):
+    """
+    based on pattern of choice, return 3-channel(RGB) image 
+    """
+    assert pattern in ['grbg', 'rggb', 'bggr', 'gbrg']
+
+    im_info = img_info(im)
+    h, w = im_info["height"], im_info["width"]
+    RGB = np.zeros((h //2, w // 2, 3))
+    
+    # index map according to bayer position
+    # upper left
+    ulxv, ulyv = np.meshgrid(np.arange(0, h, 2), np.arange(0, w, 2))
+
+    # upper right
+    urxv, uryv = np.meshgrid(np.arange(0, h, 2), np.arange(1, w, 2))
+
+    # lower left
+    llxv, llyv = np.meshgrid(np.arange(1, h, 2), np.arange(0, w, 2))
+
+    # lower right
+    lrxv, lryv = np.meshgrid(np.arange(1, h, 2), np.arange(1, w, 2))
+
+    if pattern == 'grbg':
+        RGB[:,:,0] = im[urxv.T, uryv.T]
+        RGB[:,:,1] = im[ulxv.T, ulyv.T]
+        RGB[:,:,2] = im[llxv.T, llyv.T]
+    elif pattern == 'rggb':
+        RGB[:,:,0] = im[ulxv.T, ulyv.T]
+        RGB[:,:,1] = im[urxv.T, uryv.T]
+        RGB[:,:,2] = im[lrxv.T, lryv.T]
+    elif pattern == "bggr":
+        RGB[:,:,0] = im[lrxv.T, lryv.T]
+        RGB[:,:,1] = im[urxv.T, uryv.T]
+        RGB[:,:,2] = im[ulxv.T, ulyv.T]
+    elif pattern == "gbrg":
+        RGB[:,:,0] = im[llxv.T, llyv.T]
+        RGB[:,:,1] = im[ulxv.T, ulyv.T]
+        RGB[:,:,2] = im[urxv.T, uryv.T]
+
+    return RGB
+
+def show_image(im):
+    plt.figure()
+    plt.imshow(im)
+    plt.show()
+
+
+def white_balancing_white_world(RGB):
+    """
+    white balancing based on forcing brightest object in scene to be white.
+    input: RGB image
+    output: balanced image
+    """
+    RGB_sum = np.sum(RGB, 2)
+    ind = np.unravel_index(np.argmax(RGB_sum, axis=None), RGB_sum.shape)
+    brightest = RGB[ind[0], ind[1]]
+    offset = 1.0 - brightest
+
+    return RGB + offset
+
+def white_balancing_gray_world(RGB):
+    """
+    force average color of scene to be grey
+    """
+    dim = RGB.shape[0] * RGB.shape[1] 
+    RGB_average = np.array([np.sum(RGB[:,:,0]) / dim, np.sum(RGB[:,:,1]) / dim, np.sum(RGB[:,:,2]) / dim])
+    gray = np.array([0.5, 0.5, 0.5])
+    offset = gray - RGB_average
+    balanced = RGB + offset
+    # print(np.array([np.sum(balanced[:,:,0]) / dim, np.sum(balanced[:,:,1]) / dim, np.sum(balanced[:,:,2]) / dim]))
+    return balanced
+    
+def white_balancing_preset(RGB):
+    """
+    wb based on preset
+    """
+    RGB[:,:,0] *= R_SCALE
+    RGB[:,:,1] *= G_SCALE
+    RGB[:,:,2] *= B_SCALE
+    return RGB
+
+
+BLACK = 150.
+WHITE = 4095.
+R_SCALE = 2.394531
+G_SCALE = 1.0
+B_SCALE = 1.597656
+
+
+img = read_RAW("../data/campus.tiff")
+
+img = convert2double(img)
+img = linearize(img, BLACK, WHITE)
+
+RGB = demosaicing(img)
+
+show_image(RGB)
+
+# RGB = identify_bayer(img, "rggb")
+
+
+
+# RGB = identify_bayer_dummy(img,"gbrg")
+# show_image(RGB)
+# RGB = white_balancing_white_world(RGB)
+# RGB = white_balancing_gray_world(RGB)
+# RGB = white_balancing_preset(RGB)
+# show_image(RGB)
+
+# print(img)
+# print(img_info(img))
+
+
